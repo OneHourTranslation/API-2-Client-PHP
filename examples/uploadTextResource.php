1caccd4ca5a806cdf28c446655c1cb2c6431ee83
Max Belushkin <maxb@onehourtranslation.com>
2013-01-16 16:30:33 +0200

    Ticket #1070: PHP lib - don't use constants for public/secret key [completed:1070]

--- examples/uploadTextResource.php
+++ examples/uploadTextResource.php
@@ -3,8 +3,8 @@
 require_once '../lib/OHTAPI.php';
 try {
     OHTAPI::config(array(
-        'public_key' => OHT_API_PUBLIC_KEY,
-        'secret_key' => OHT_API_SECRET_KEY,
+        'public_key' => '<public key here>',
+        'secret_key' => '<secret key here>',
         'sandbox' => true
     ));
     $oht = OHTAPI::getInstance();
